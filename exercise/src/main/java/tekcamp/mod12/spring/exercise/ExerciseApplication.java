a86d0ab9e7b542654e8e13de5d3e2028cc3a19c4
John Kol <Kolja0@protonmail.com>
2020-07-28 00:46:30 -0500

    Start point

---
+++ exercise/src/main/java/tekcamp/mod12/spring/exercise/ExerciseApplication.java
@@ -0,0 +1,21 @@
+package tekcamp.mod12.spring.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
