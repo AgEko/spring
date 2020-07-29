97b1051ab64600afd182a3771733f754a06a2a3d
John Kol <Kolja0@protonmail.com>
2020-07-29 01:13:08 -0500

    Created User.java

--- exercise/src/main/java/tekcamp/mod12/spring/exercise/ExerciseApplication.java
+++ exercise/src/main/java/tekcamp/mod12/spring/exercise/ExerciseApplication.java
@@ -2,8 +2,6 @@ package tekcamp.mod12.spring.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,8 +12,5 @@ public class ExerciseApplication {
 		SpringApplication.run(ExerciseApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+
 }
---
+++ exercise/src/main/java/tekcamp/mod12/spring/exercise/Model/User.java
@@ -0,0 +1,65 @@
+package tekcamp.mod12.spring.exercise.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    //Todo needs encryption:
+    private String password;
+
+    public User(){
+    }
+
+    public User(String firstName, String lastName, String address, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
